bf85ab3be8de35cd267f2f3c8ffb00d6f0f112e7
Martin Cardozo <tiin.-@hotmail.es>
2023-09-02 15:11:09 -0300

    se protege las rutas

--- js/index/app.js
+++ js/index/app.js
@@ -6,7 +6,12 @@ const sectionContactos = document.getElementById('section-contactos');
 const alert = document.getElementById('alertResultado')
 
 
- 
+const estaLogueado = JSON.parse(sessionStorage.getItem('estaLogueado'));
+if (!estaLogueado) {
+  // No deberia poder ver la página
+  window.location.href = './login.html';
+}
+
 
 contactos.forEach(element => {
     crearTarjetaDeContactos(element);
